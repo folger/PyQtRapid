4628ed978d3bc0767ad1c5f1029f2f438d0b7edb
folger <folger6@gmail.com>
2014-02-16 22:01:17 +0800

    update makeui.sh

--- MakePyQt/makeui.sh
+++ MakePyQt/makeui.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-for f in $(ls); do
-  if [ "${f:${#f}-3:3}" == ".ui" ]; then
-    n=ui_${f:0:${#f}-3}.py
+# for f in $(ls); do
+#   if [ "${f:${#f}-3:3}" == ".ui" ]; then
+#     n=ui_${f:0:${#f}-3}.py
 
-    echo "Converting $f to $n"
-    pyuic4 $f -o $n
-  fi
+#     echo "Converting $f to $n"
+#     pyuic4 $f -o $n
+#   fi
+# done
+
+for f in $(find . -name "*.ui"); do
+  filename=$(basename $f)
+  name="${filename%.*}"
+  n=$(dirname $f)/ui_$name.py
+  echo "Converting $f to $n"
+  pyuic4 $f -o $n
 done
 
